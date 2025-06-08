bc7658d18ac3ed1ccedf59d7ddebe54cbec3acba
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2025-06-08 23:08:24 +0800

    added lazyloading for fast loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,51 @@
-import Contact from "./components/contact/Contact"
-import Hero from "./components/hero/Hero"
-import Portfolio from "./components/portfolio/Portfolio"
-import Services from "./components/services/Services"
+
+import { lazy, Suspense } from "react"
+
+const Hero = lazy(() => import("./components/hero/Hero"));
+const Services = lazy(() => import("./components/services/Services"));
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import  Lazyload  from "react-lazyload";
 
 const App = () => {
   return (
     <div className='container'>
+
+      <Suspense fallback={"Loading..."}>
+      <Lazyload height="100vh" offset={-100}>
       
         <section id='home'>
           <Hero />
         </section>
 
+         </Lazyload>
+         </Suspense>
+
+ <Suspense fallback={"Loading..."}>
+      <Lazyload height="100vh" offset={-100}>
         <section id='services'>
           <Services />
         </section>
-     
+     </Lazyload>
+         </Suspense>
    
+   <Suspense fallback={"Loading..."}>
+      <Lazyload height="600vh" offset={-100}>
       <Portfolio />
    
+ </Lazyload>
+         </Suspense>
 
+           <Suspense fallback={"Loading..."}>
+      <Lazyload height="100vh" offset={-100}>
         <section id='contact'>
           <Contact />
         </section>
+
+         </Lazyload>
+         </Suspense>
+
+       
          
     </div>
   )
